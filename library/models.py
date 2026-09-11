7c70b3a26a82f11668451047ce54cab2993ce85e
mijaelino21-debug <mijaelino21@gmail.com>
2026-09-11 15:24:46 -0500

    feat: modelos base Author, Book, Publisher, Category

--- library/models.py
+++ library/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Publisher'
+        verbose_name_plural = 'Publishers'
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
+    def __str__(self):
+        return self.title
