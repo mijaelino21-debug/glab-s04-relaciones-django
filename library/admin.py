db8cbbb70e455f05550182fe9265cf4f3ea04b71
mijaelino21-debug <mijaelino21@gmail.com>
2026-09-11 15:37:02 -0500

    feat: configuracion del admin para library

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Author, AuthorProfile, Category, Publisher, Book, Publication
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(AuthorProfile)
+class AuthorProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    pass
+
+class PublicationInline(admin.TabularInline):
+    model = Publication
+    extra = 1
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    filter_horizontal = ('categories',)
+    inlines = [PublicationInline]
